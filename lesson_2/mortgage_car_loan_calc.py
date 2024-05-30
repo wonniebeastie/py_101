ba35b0189b70d953dd7f99a97062c76ece9ca42e
Wonnie Ki <wonnie.tech@gmail.com>
2024-05-30 12:24:21 -0700

    Move around & rename a bunch of files
    Not sure why I thought to make a virtual environment for all the
    projects separately, but changed it to just one VE for lesson 2
    and created a single pylint config file for the lesson_2 directory.

---
+++ lesson_2/mortgage_car_loan_calc.py
@@ -0,0 +1,25 @@
+# Create a mortgage/car loan calculator that determines the monthly payment 
+# assuming that interest is compounded monthly.
+
+# Needed Info:
+# - The loan amount
+# - The Annual Percentage Rate (APR)
+# - The loan duration
+
+# From the above, calcuate:
+# - monthly interest rate (APR / 12)
+# - loan duration in months
+
+# Python formula:
+# m = p * (j / (1 - (1 + j) ** (-n)))
+
+# m = monthly payment
+# p = loan amount
+# j = monthly interest rate
+# n = loan duration in months
+
+
+# PEDAC
+
+# STEP 1: Understand the Problem
+# INPUTS: 
