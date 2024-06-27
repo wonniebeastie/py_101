4c48d03710bb628a1168e8b3e3dca378f33b7f1a
Wonnie Ki <wonnie.tech@gmail.com>
2024-06-27 13:13:08 -0700

    Do easy problem #4, correct answer

---
+++ lesson_3/easy_1/4_capitalize_first.py
@@ -0,0 +1,9 @@
+munsters_description = "the Munsters are CREEPY and Spooky."
+# Turn it into this: => 'The munsters are creepy and spooky.'
+
+print(munsters_description.casefold().capitalize())
+
+# Did not need to method chain as capitalize() method already returns
+# the rest of the string in lowercase.
+
+# Lesson: Read documentation better.
