d5759adcca2cf35e165fc839d200a854d2c8505e
Wonnie Ki <wonnie.tech@gmail.com>
2024-06-27 13:55:57 -0700

    Finish Easy 1 problems

---
+++ lesson_3/easy_1/5_swap_cases.py
@@ -0,0 +1,6 @@
+munsters_description = "The Munsters are creepy and spooky."
+
+# print the string with the case of all letters swapped:
+# "tHE mUNSTERS ARE CREEPY AND SPOOKY."
+
+print(munsters_description.swapcase())
